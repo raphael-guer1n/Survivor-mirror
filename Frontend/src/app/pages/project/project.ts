708c76135377e7f2df2a1334d2510da2a83f26dd
LixiosDelios <alexandre.guillaud@epitech.eu>
2025-09-01 17:31:24 +0200

    feat(frontend/project): add project page component

---
+++ Frontend/src/app/pages/project/project.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project',
+  imports: [],
+  standalone: true,
+  templateUrl: './project.html',
+  styleUrl: './project.css'
+})
+export class Project {
+
+}
