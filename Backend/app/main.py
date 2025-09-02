5dbf85671e539abff0b586eb55054222b3bf7531
raphael <raphael.guerin@epitech.eu>
2025-09-02 16:27:42 +0200

    feat(Routers): include events routers in main.py

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from app.routers import users
 from app.routers import auth
+from app.routers import events
 
 app = FastAPI()
 
 app.include_router(users.router, prefix="/api", tags=["Users"])
 app.include_router(auth.router)
+app.include_router(events.router, prefix="/api", tags=["Events"])
 
 @app.get("/")
 async def root():
