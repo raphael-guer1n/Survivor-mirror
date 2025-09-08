10c95bb8652882484cd27678635539cf7e5cf06e
Raoul <raoul.bardinet@epitech.eu>
2025-09-08 10:32:35 +0200

    (Backend/communication/privates_messages style): file is cleaner

--- Backend/app/communication/privates_messages.py
+++ Backend/app/communication/privates_messages.py
@@ -18,28 +18,37 @@ def send_message(message: Message):
         print(table)
 
     try:
-        cursor.execute("SELECT id FROM users where email = %s", (sender_email,))
+        cursor.execute("SELECT id FROM users where email = %s",
+            (sender_email,))
         get_sender = cursor.fetchone()
         if not get_sender:
-            raise HTTPException(status_code=501, detail="Sender does not exist.")
-        cursor.execute("SELECT id FROM users where email = %s", (reciver_email,))
+            raise HTTPException(status_code=501,
+                detail="Sender does not exist.")
+        cursor.execute("SELECT id FROM users where email = %s",
+            (reciver_email,))
         get_reciver = cursor.fetchone()
         if not get_reciver:
-            raise HTTPException(status_code=501, detail="The email you tried to send this message does not exist.")
+            raise HTTPException(status_code=501,
+                detail="The email you tried to send this message does not exist.")
         if (get_sender == get_reciver):
-            raise HTTPException(status_code=501, detail="Sender and Reciver of the massages cannot be the same.")
+            raise HTTPException(status_code=501,
+                detail="Sender and Reciver of the massages cannot be the same.")
         reciver_id: int = get_reciver['id']
         sender_id: int = get_sender['id']
-        cursor.execute("SELECT id FROM conversations where user1_id = %s AND user2_id = %s OR user1_id = %s AND user2_id = %s", (sender_id, reciver_id, reciver_id, sender_id,))
+        cursor.execute("SELECT id FROM conversations where user1_id = %s AND user2_id = %s OR user1_id = %s AND user2_id = %s",
+            (sender_id, reciver_id, reciver_id, sender_id,))
         get_conv = cursor.fetchone()
         if not get_conv:
-            cursor.execute("INSERT INTO conversations (user1_id, user2_id) VALUES (%s, %s)",(sender_id, reciver_id,))
+            cursor.execute("INSERT INTO conversations (user1_id, user2_id) VALUES (%s, %s)",
+                (sender_id, reciver_id,))
             connection.commit()
-            cursor.execute("SELECT id FROM conversations where user1_id = %s AND user2_id = %s OR user1_id = %s AND user2_id = %s", (sender_id, reciver_id, reciver_id, sender_id,))
+            cursor.execute("SELECT id FROM conversations where user1_id = %s AND user2_id = %s OR user1_id = %s AND user2_id = %s",
+                (sender_id, reciver_id, reciver_id, sender_id,))
             get_conv = cursor.fetchone()
         conv_id: int = get_conv['id']
         cursor.execute(
-            "INSERT INTO messages (conversation_id, sender_id, receiver_id, content) VALUES (%s, %s, %s, %s)", (conv_id, sender_id, reciver_id, message.content_message,))
+            "INSERT INTO messages (conversation_id, sender_id, receiver_id, content) VALUES (%s, %s, %s, %s)",
+            (conv_id, sender_id, reciver_id, message.content_message,))
         connection.commit()
         return {"sender_email": sender_email, "reciver_email": reciver_email, "content_message": message.content_message}
     finally:
