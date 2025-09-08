9322499e1cb6a5d89f77b9d15cee8951fe615ad4
Raoul <raoul.bardinet@epitech.eu>
2025-09-08 11:12:17 +0200

    feat(Backend/communication/private_communication): add of the getters for conversation and last message

--- Backend/app/communication/privates_messages.py
+++ Backend/app/communication/privates_messages.py
@@ -157,3 +157,102 @@ def read_message(reader: Read_conversation_from):
     finally:
         cursor.close()
         connection.close()
+        
+@comm.get("/read_message", response_model= Read_message)
+def read_message(reader_email :str, readed_email: str):
+    connection = get_connection()
+    cursor = connection.cursor(dictionary=True)
+    conversation: list[Read_message]= []
+    try:
+        cursor.execute("SELECT id, name FROM users where email = %s",
+            (reader_email,))
+        get_reader = cursor.fetchone()
+        if not get_reader:
+            raise HTTPException(status_code=501,
+                detail="Reader does not exist.")
+        cursor.execute("SELECT id, name FROM users where email = %s",
+            (readed_email,))
+        get_readed = cursor.fetchone()
+        if not get_readed:
+            raise HTTPException(status_code=501,
+                detail="The email you tried to read your conversation with does not exist.")
+        if (get_reader == get_readed):
+            raise HTTPException(status_code=501,
+                detail="You cannot try to read a conversation with yourself.")
+        reader_id: int = get_reader['id']
+        readed_id: int = get_readed['id']
+        reader_name: str = get_reader['name']
+        readed_name: str = get_readed['name']
+        cursor.execute(
+            "SELECT id FROM conversations where user1_id = %s AND user2_id = %s OR user1_id = %s AND user2_id = %s",
+            (reader_id, readed_id, readed_id, reader_id,))
+        get_conv = cursor.fetchone()
+        if get_conv:
+            conv_id: int = get_conv['id']
+            cursor.execute("SELECT sender_id, content FROM messages where conversation_id = %s",
+                (conv_id,))
+            all_messages = cursor.fetchall()
+            last_message = all_messages[-1]
+            if reader_id == last_message['sender_id']:
+                return {"sender_name": reader_name, "content": last_message['content']}
+            if readed_id == last_message['sender_id']:
+                return {"sender_name": readed_name, "content": last_message['content']}
+
+        else:
+            raise HTTPException(status_code=501, 
+                detail="No conversation with this email yet.")
+    finally:
+        cursor.close()
+        connection.close()
+        
+        
+@comm.get("/read_conversation", response_model= list[Read_message])
+def read_message(reader_email: str, readed_email: str):
+    connection = get_connection()
+    cursor = connection.cursor(dictionary=True)
+    conversation: list[Read_message]= []
+    try:
+        cursor.execute("SELECT id, name FROM users where email = %s",
+            (reader_email,))
+        get_reader = cursor.fetchone()
+        if not get_reader:
+            raise HTTPException(status_code=501,
+                detail="Reader does not exist.")
+        cursor.execute("SELECT id, name FROM users where email = %s",
+            (readed_email,))
+        get_readed = cursor.fetchone()
+        if not get_readed:
+            raise HTTPException(status_code=501,
+                detail="The email you tried to read your conversation with does not exist.")
+        if (get_reader == get_readed):
+            raise HTTPException(status_code=501,
+                detail="You cannot try to read a conversation with yourself.")
+        reader_id: int = get_reader['id']
+        readed_id: int = get_readed['id']
+        reader_name: str = get_reader['name']
+        readed_name: str = get_readed['name']
+        cursor.execute(
+            "SELECT id FROM conversations where user1_id = %s AND user2_id = %s OR user1_id = %s AND user2_id = %s",
+            (reader_id, readed_id, readed_id, reader_id,))
+        get_conv = cursor.fetchone()
+        if get_conv:
+            conv_id: int = get_conv['id']
+            cursor.execute("SELECT sender_id, content FROM messages where conversation_id = %s",
+                (conv_id,))
+            all_messages = cursor.fetchall()
+            for message_s in all_messages:
+                if reader_id == message_s['sender_id']:
+                    conversation.append(Read_message(
+                        sender_name=reader_name,
+                        content=message_s['content']))
+                if readed_id == message_s['sender_id']:
+                    conversation.append(Read_message(
+                        sender_name=readed_name,
+                        content=message_s['content']))
+            return conversation
+        else:
+            raise HTTPException(status_code=501, 
+                detail="No conversation with this email yet.")
+    finally:
+        cursor.close()
+        connection.close()
