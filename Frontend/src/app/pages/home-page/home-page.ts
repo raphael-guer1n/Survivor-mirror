0c347b01d1776747695625d59151d0915732eafe
Alexis <alexis.constantinopoulos@epitech.eu>
2025-09-10 16:09:56 +0200

    feat(home-page): implement stats loading logic for startups, partners, and events

--- Frontend/src/app/pages/home-page/home-page.ts
+++ Frontend/src/app/pages/home-page/home-page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-home',
@@ -8,5 +10,27 @@ import {RouterLink} from '@angular/router';
   templateUrl: './home-page.html',
   styleUrl: './home-page.css'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  startupsCount: number|null = null;
+  partnersCount: number|null = null;
+  eventsCount: number|null = null;
+  loadingStats = true;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadingStats = true;
+    Promise.all([
+      this.http.get<any[]>('/api/startups/').toPromise(),
+      this.http.get<any[]>('/api/partners/').toPromise(),
+      this.http.get<any[]>('/api/events/').toPromise(),
+    ]).then(([startups, partners, events]) => {
+      this.startupsCount = (startups ?? []).length;
+      this.partnersCount = (partners ?? []).length;
+      this.eventsCount = (events ?? []).length;
+      this.loadingStats = false;
+    }).catch(() => {
+      this.loadingStats = false;
+    });
+  }
 }
